62127fe36f9f13af7a60a65568a7ee36503fdd3f
Bryann Valderrama <bryanttandrei2015@gmail.com>
2024-04-24 15:37:53 -0500

    fix: import xblock class in test

--- tests/test_discussion_grading.py
+++ tests/test_discussion_grading.py
@@ -6,7 +6,7 @@ from django.test import TestCase
 from xblock.fields import ScopeIds
 from xblock.test.toy_runtime import ToyRuntime
 
-from my_xblock import MyXBlock
+from discussion_grading import XBlockDiscussionGrading
 
 
 class TestXBlockDiscussionGrading(TestCase):
@@ -14,7 +14,7 @@ class TestXBlockDiscussionGrading(TestCase):
     def test_my_student_view(self):
         """Test the basic view loads."""
         scope_ids = ScopeIds('1', '2', '3', '4')
-        block = MyXBlock(ToyRuntime(), scope_ids=scope_ids)
+        block = XBlockDiscussionGrading(ToyRuntime(), scope_ids=scope_ids)
         frag = block.student_view()
         as_dict = frag.to_dict()
         content = as_dict['content']
